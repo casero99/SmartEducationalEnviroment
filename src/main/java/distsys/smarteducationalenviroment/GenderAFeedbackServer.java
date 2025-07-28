0c29a2cbd1c2990fbf96f3a77b933b9799a32ae9
Carolina <caro.serranoroyval@gmail.com>
2025-07-28 12:29:53 +0100

    genderAFeedback working

--- src/main/java/distsys/smarteducationalenviroment/GenderAFeedbackServer.java
+++ src/main/java/distsys/smarteducationalenviroment/GenderAFeedbackServer.java
@@ -13,7 +13,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*@author Carolina*/
-
 public class GenderAFeedbackServer extends GenderAFeedbackImplBase {
 
     private static final Logger logger = Logger.getLogger(DomesticActSimulatorServer.class.getName());
@@ -43,52 +42,51 @@ public class GenderAFeedbackServer extends GenderAFeedbackImplBase {
 
     //public void taskPerformance(StudentTask request, StreamObserver<TaskFeedbackSummary> responseObserver){
     public StreamObserver<StudentTask> taskPerformance(StreamObserver<TaskFeedbackSummary> responseObserver) {
-        return new StreamObserver<StudentTask>(){
-        
-                int totalDuration = 0;
-                int taskCount = 0;
-                StringBuilder feedbackBuilder = new StringBuilder();
-                
-                
-                public void onNext(StudentTask task){
-                    String studentName = task.getStudentName();
+        return new StreamObserver<StudentTask>() {
+
+            int totalDuration = 0;
+            int taskCount = 0;
+            StringBuilder feedbackBuilder = new StringBuilder();
+
+            public void onNext(StudentTask task) {
+                String studentName = task.getStudentName();
                 String studentTaks = task.getStudentTask();
                 int taskDuration = task.getTaskDuration();
-                
+
                 totalDuration += taskDuration;
                 taskCount++;
-                
+
                 String msg;
-                if (taskDuration <= 5){
+                if (taskDuration <= 5) {
                     msg = "Fast, efficient, and done pretty well! (:";
-                }else if(taskDuration <= 10){
+                } else if (taskDuration <= 10) {
                     msg = "Okay. Have the task done.";
-                }else{
-                    msg="Too slow. Try to speed up";
+                } else {
+                    msg = "Too slow. Try to speed up";
                 }
-                
-                feedbackBuilder.append("Good").append(studentName).append(" did ").append(studentTask)
-                .append(" in ").append(taskDuration).append(" seconds ").append(msg).append("\n");
-                
+
+                feedbackBuilder.append("Good").append(studentName).append(" did ").append(studentTaks)
+                        .append(" in ").append(taskDuration).append(" seconds ").append(msg).append("\n");
+
                 System.out.println("Received task from " + studentName);
-                }
-                
-                public void onError(Throwable t){
-                logger.log(level.WARNING, "Encountered error in recordRoute", t);
-                }
-                
-                public void onCompleted(){
+            }
+
+            public void onError(Throwable t) {
+                logger.log(Level.WARNING, "Encountered error in recordRoute", t);
+            }
+
+            public void onCompleted() {
                 int totalTaskDuration = (taskCount == 0) ? 0 : totalDuration / taskCount;
-                
+
                 TaskFeedbackSummary summary = TaskFeedbackSummary.newBuilder()
-                .setTotalTask(taskCount)
-                .setTotalTime(totalTaskDuration)
-                .setSummary(feedbackBuilder.toString())
-                .build();
-                }
-                }
+                        .setTotalTask(taskCount)
+                        .setTotalTime(totalTaskDuration)
+                        .setSummary(feedbackBuilder.toString())
+                        .build();
+            }
+        };
                 
-                /*
+                }           /*
         String[] insights = {
         "The student " + studentName + " is showing gender inbalance in participation.",
         "Consider rotating household task equally between the students.",
@@ -109,11 +107,9 @@ public class GenderAFeedbackServer extends GenderAFeedbackImplBase {
         responseObserver.onCompleted();
     }
                 
-    */
-
-    //Bi-directional RPC
-    @Override;
-
+                
+    
+    */ //Bi-directional RPC
     public StreamObserver<StudentTask> liveTaskFeedback(final StreamObserver<TaskFeedback> responseObserver) {
         return new StreamObserver<StudentTask>() {
 
@@ -121,24 +117,29 @@ public class GenderAFeedbackServer extends GenderAFeedbackImplBase {
             public void onNext(StudentTask event) {
                 //aqui va la logica de bi-directional streaming
                 //mandar respuestas a tiempo real(responseObserver.onNext(...))
+                //extract the data sent by the client
+                String studentName = event.getStudentName();
                 String studentTask = event.getStudentTask();
                 double taskDuration = event.getTaskDuration();
-                String feedback;
+                String feedback; //preguntar acerca de este
 
+                //feedback logic based on task duration
                 if (taskDuration > 5.0) {
                     feedback = "Great Job! But try to be a little bit faster completing the task" + studentTask;
                 } else {
                     feedback = "You've done amazing! Keep it that way!" + studentTask;
                 }
 
+                //build the reponse message managed by feedback
                 TaskFeedback response = TaskFeedback.newBuilder().setStudentName(studentName).setFeedback(feedback).build();
 
+                //real time feedback to client
                 responseObserver.onNext(response);
             }
 
             @Override
             public void onError(Throwable t) {
-                logger.log(Level.WARNING, "Encountered error in routeChat", t);
+                logger.log(Level.WARNING, "Encountered error in liveTaskFeedback", t);
 
             }
 
