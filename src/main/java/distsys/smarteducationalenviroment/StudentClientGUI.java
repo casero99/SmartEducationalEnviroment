0f95ae13f172e62697df4b399f6ba5a6e5b35d9a
Carolina <caro.serranoroyval@gmail.com>
2025-07-30 03:15:17 +0100

    Swing GUI buttons

--- src/main/java/distsys/smarteducationalenviroment/StudentClientGUI.java
+++ src/main/java/distsys/smarteducationalenviroment/StudentClientGUI.java
@@ -1,10 +1,12 @@
-
 package distsys.smarteducationalenviroment;
 
+
+import generated.grpc.analyzer.Student;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashSet;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -17,109 +19,178 @@ import javax.swing.JTextField;
 
 /* @author Carolina*/
 
-/*
+ /*
     This class is the graphical window of my program.
-It will show fields, buttons, and outputs.
+It will show fields, buttons, and outputs!
 
-*/
+ */
 public class StudentClientGUI {
+
     public static void main(String[] args) {
-        
+
+        //*********************************************************
+        //SERVICE 1. UNARY - Domestic Activity Simulator
+        //*********************************************************
         //main window (JFrame)
-        JFrame jframe = new JFrame("Eco-classroom client");
+        JFrame jframe = new JFrame("Domestic Simulator");
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setSize(500, 400);
         jframe.setLayout(new BorderLayout());
-        
+
         // **************TOP PANEL FOR INPUT****************
-        JPanel inPanel = new JPanel(new GridLayout(5,2,10,10));
-        
+        JPanel inPanel = new JPanel(new GridLayout(4, 2, 10, 10));
+
         JTextField fName = new JTextField();
         JTextField fAge = new JTextField();
-        
-        String[] fGender = {"-Select gender-","Male", "Female", "Other"};
+
+        String[] fGender = {"-Select gender-", "Male", "Female", "Other"};
         JComboBox<String> genderDd = new JComboBox<>(fGender);
-        
+
         String[] tasks = {"-Select task to do-", "Washing Dishes", "Sweeping", "Mooping", "Laundry", "Cooking", "Ironing", "Make the bed"};
         JComboBox<String> taskDd = new JComboBox<>(tasks);
-        
+
         inPanel.add(new JLabel("Student Name: "));
         inPanel.add(fName);
-        
+
         inPanel.add(new JLabel("Student Age: "));
         inPanel.add(fAge);
-        
+
         inPanel.add(new JLabel("Student Gender: "));
         inPanel.add(genderDd);
-        
+
         inPanel.add(new JLabel("Select Task to do: "));
         inPanel.add(taskDd);
         
-        JButton subButton = new JButton("Submit Domestic Task");
-        
         inPanel.add(new JLabel()); //empty cell
-        inPanel.add(subButton);
-        
+
         //************** CENTER PANEL FOR OUTPUT *****************
         JTextArea outputArea = new JTextArea();
         outputArea.setEditable(false);
         JScrollPane pScroll = new JScrollPane(outputArea);
         
+        //************** BOTTOM PANEL BUTTONS*****************
+        JButton subButton = new JButton("Submit Domestic Task");
+        JButton analyzeButton = new JButton("Submit AnalizeParticipation Task");
+        
+        JPanel pButton = new JPanel(); //default FlowLayout
+        pButton.add(subButton);
+        pButton.add(analyzeButton);
         //***************** ADD PANELS TO FRAME ******************
         jframe.add(inPanel, BorderLayout.NORTH);
         jframe.add(pScroll, BorderLayout.CENTER);
-        
+
         //************** BUTTON LOGIC "PLACEHOLDER" **************
-        subButton.addActionListener(new ActionListener(){
-            
+        subButton.addActionListener(new ActionListener() {
+
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 String name = fName.getText();
                 String ageStr = fAge.getText();
-                
-                 //message will be displayed if the user leaves the spaces empty.
-                    if(name.isEmpty() || ageStr.isEmpty()){
-                        JOptionPane.showMessageDialog(null, "Please fill all the boxes.");
-                        return;
-                    }
-                    
-                 //age from an int to a string
-                 int age;
-                 try{
-                     age = Integer.parseInt(ageStr);
-                     
-                 }catch(NumberFormatException ex){
-                     JOptionPane.showMessageDialog(null, "Age must be a number");
-                     return;
-                 }
-                
+
+                //message will be displayed if the user leaves the spaces empty.
+                if (name.isEmpty() || ageStr.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Please fill all the boxes.");
+                    return;
+                }
+
+                //age from an int to a string
+                int age;
+                try {
+                    age = Integer.parseInt(ageStr);
+
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Age must be a number");
+                    return;
+                }
+
                 String gender = (String) genderDd.getSelectedItem();
-                
-                    //message will be displayed if the user doesn't select a gender.
-                    if(gender.equals("-Select gender-")){
-                        JOptionPane.showMessageDialog(null, "Please select a valid gender");
-                        return;
-                    }
-                
-                String task = (String) taskDd.getSelectedItem();
-                
+
                 //message will be displayed if the user doesn't select a gender.
-                    if(task.equals("-Select task to do-")){
-                        JOptionPane.showMessageDialog(null, "Please select a valid task");
-                        return;
-                    }
-                    
-                    //calling the gRPC helper method
-                    String result = StudentClientHelper.runUnaryDomesticTask(name, age, gender, task);
-                
+                if (gender.equals("-Select gender-")) {
+                    JOptionPane.showMessageDialog(null, "Please select a valid gender");
+                    return;
+                }
+
+                String task = (String) taskDd.getSelectedItem();
+
+                //message will be displayed if the user doesn't select a gender.
+                if (task.equals("-Select task to do-")) {
+                    JOptionPane.showMessageDialog(null, "Please select a valid task");
+                    return;
+                }
+
+                //calling the gRPC helper method
+                String result = StudentClientHelper.runUnaryDomesticTask(name, age, gender, task);
+
                 outputArea.append("Submitting task...\n");
                 outputArea.append("Student Name: " + name + "\n Age: " + age + "\n Gender: " + gender + "\n Task to do: " + task + "\n");
                 outputArea.append("!!!! " + result + "\n");
             }
         });
-        
+
         //************** SHOW THE WINDOW **************************
         jframe.setVisible(true);
+
+        //*********************************************************
+        //SERVER 2. Server Streaming RPC - Participation Analizer
+        //*********************************************************
+        JButton analyzeButton = new JButton("Analyze Participation");
+        JPanel subButton2 = new JPanel();
+        subButton2.add(analyzeButton);
+        analyzeButton.addActionListener(e -> {
+
+            //gather input from fields and validating inputs
+            String name = fName.getText().trim();
+            String ageStr = fAge.getText().trim();
+
+            //message will be displayed if the user leaves the spaces empty.
+            if (name.isEmpty() || ageStr.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please fill all the boxes.");
+                return;
+            }
+
+            //age from an int to a string
+            int age;
+            try {
+                age = Integer.parseInt(ageStr);
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Age must be a number");
+                return;
+            }
+
+            String gender = (String) genderDd.getSelectedItem();
+
+            //message will be displayed if the user doesn't select a gender.
+            if (gender.equals("-Select gender-")) {
+                JOptionPane.showMessageDialog(null, "Please select a valid gender");
+                return;
+            }
+
+            String task = (String) taskDd.getSelectedItem();
+
+            //message will be displayed if the user doesn't select a gender.
+            if (task.equals("-Select task to do-")) {
+                JOptionPane.showMessageDialog(null, "Please select a valid task");
+                return;
+            }
+
+            //Building student message
+            Student student = Student.newBuilder()
+                    .setStudentName(name)
+                    .setStudentAge(age)
+                    .setGender(gender)
+                    .setTaskName(task)
+                    .build();
+
+            //calling 'helper' to run server-streaming RPC
+            String feedback = StudentClientHelper.runServerStreamingParticipationAnalizer(student);
+            outputArea.append("!!!Server replied:\n " + feedback + "\n");
+
+        });
+
     }
-    
-} 
+
+}
+
+
--- src/main/java/distsys/smarteducationalenviroment/StudentClientHelper.java
+++ src/main/java/distsys/smarteducationalenviroment/StudentClientHelper.java
@@ -1,69 +1,116 @@
-
 package distsys.smarteducationalenviroment;
 
+import generated.grpc.analyzer.ParticipationAnalizerGrpc;
+import generated.grpc.analyzer.ParticipationRequest;
+import generated.grpc.analyzer.ParticipationStatistics;
 import generated.grpc.domestic.DomesticActSimulatorGrpc;
 import generated.grpc.domestic.RegisterStudentsRequest;
 import generated.grpc.domestic.ResisterStudentsReply;
 import generated.grpc.domestic.Student;
+//import generated.grpc.analyzer.Student;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Iterator;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
 /* @author Carolina*/
 
-/*
+ /*
     This class basically is a 'helper class' 
     that contains the gRPC connection logic.
 
-*/
+ */
 public class StudentClientHelper {
-    
-    public static String runUnaryDomesticTask(String name, int age, String gender, String task){
-        
-        try{
+
+    public static String runUnaryDomesticTask(String name, int age, String gender, String task) {
+
+        try {
             //discover the domestic via jmDns
             JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
             ServiceInfo serviceInfo = jmdns.getServiceInfo("grpc.tcp.local.", "DomesticService", 50051);
-        
-            if(serviceInfo == null){
+
+            if (serviceInfo == null) {
                 return "****************Could not find DomesticService via jmDNS";
             }
-            
+
             String host1 = serviceInfo.getInetAddresses()[0].getHostAddress();
             int port1 = serviceInfo.getPort();
-            
+
             //Creating gRPC channel and stub
-            
             ManagedChannel channel = ManagedChannelBuilder.forAddress(host1, port1)
                     .usePlaintext()
                     .build();
-            
-            DomesticActSimulatorGrpc.DomesticActSimulatorBlockingStub stub = 
-                    DomesticActSimulatorGrpc.newBlockingStub(channel);
-            
+
+            DomesticActSimulatorGrpc.DomesticActSimulatorBlockingStub stub
+                    = DomesticActSimulatorGrpc.newBlockingStub(channel);
+
             //create request --- get reply
-            Student student = Student.newBuilder()
+            Student students = Student.newBuilder()
                     .setStudentName(name)
                     .setStudentAge(age)
                     .setGender(gender)
                     .setTaskName(task)
                     .build();
-            
-             RegisterStudentsRequest request = RegisterStudentsRequest.newBuilder()
-                     .addStudents(student)
-                     .build();
-        
+
+            RegisterStudentsRequest request = RegisterStudentsRequest.newBuilder()
+                    .addStudents(students)
+                    .build();
+
             ResisterStudentsReply reply = stub.registerStudents(request);
-            
+
             channel.shutdown();
-            
-            return "Feedback: "+ reply.getMessage();
-            
-        }catch(IOException e){
+
+            return "Feedback: " + reply.getMessage();
+
+        } catch (IOException e) {
+            return "*****************Error during gRPC request: " + e.getMessage();
+        }
+    }
+
+    public static String runServerStreamingParticipationAnalizer(generated.grpc.analyzer.Student student) {
+        try {
+            //discover the domestic via jmDns
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+            ServiceInfo serviceInfo = jmdns.getServiceInfo("grpc.tcp.local.", "ParticipationAnalizer", 50052);
+
+            if (serviceInfo == null) {
+                return "****************Could not find ParticipationAnalizer via jmDNS";
+            }
+
+            String host2 = serviceInfo.getInetAddresses()[0].getHostAddress();
+            int port2 = serviceInfo.getPort();
+
+            //Creating gRPC channel and stub
+            ManagedChannel channel = ManagedChannelBuilder.forAddress(host2, port2)
+                    .usePlaintext()
+                    .build();
+
+            ParticipationAnalizerGrpc.ParticipationAnalizerBlockingStub stub
+                    = ParticipationAnalizerGrpc.newBlockingStub(channel);
+
+            //Create the request
+            ParticipationRequest request = ParticipationRequest.newBuilder()
+                    .addStudents(student) //expecting a list, but it can be only one student
+                    .build();
+            //send request and receive streamed responses
+            Iterator<ParticipationStatistics> replies = stub.analyzerParticipation(request);
+
+            //create the iterator for the server stream
+            StringBuilder resultBuilder = new StringBuilder();
+            while (replies.hasNext()) {
+                ParticipationStatistics reply = replies.next();
+                resultBuilder.append(" . ").append(reply.getSummary()).append("\n");
+            }
+
+            channel.shutdown();
+            return resultBuilder.toString();
+
+        } catch (IOException e) {
             return "*****************Error during gRPC request: " + e.getMessage();
         }
     }
 }
+
